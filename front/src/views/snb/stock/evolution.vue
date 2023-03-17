b273f4cdf3aab8ec428a548af1affb922f50e986
Arif Stephane <hounkpatinstepane@gmail.com>
2023-03-17 16:31:46 +0100

    fix(font): add button back & add name engin

--- front/src/views/snb/stock/evolution.vue
+++ front/src/views/snb/stock/evolution.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="">
-    <div class="space-y-6">
+    <router-link :to="{ name: 'stock' }" path="stock">
+      <BaseGoBack> </BaseGoBack>
+    </router-link>
+    <div class="space-y-6 mt-3">
       <div class="">
         <BaseTitle title="Évolution du stock"></BaseTitle>
         <!-- Panel -->
         <div class="flex mt-8 space-x-4">
           <div v-for="(etat, index) in etats" :key="index">
-            <VPanel :labels="etat.name" @click="activeTab = etat.value" :class="{
-              'text-white bg-primary': etat.value === activeTab,
-            }" />
+            <VPanel
+              :labels="etat.name"
+              @click="activeTab = etat.value"
+              :class="{
+                'text-white bg-primary': etat.value === activeTab,
+              }"
+            />
           </div>
         </div>
       </div>
@@ -17,29 +24,32 @@
         <TimeLineCard v-for="item in 6" />
       </div>
     </div>
-
   </div>
 </template>
 
 <script setup lang="ts">
 import VPanel from "@/components/VPanel.vue";
-import { StockState } from "@/types/enumerations"
-import TimeLineCard from "@/components/TimeLine/TimeLineCard.vue"
+import { StockState } from "@/types/enumerations";
+import TimeLineCard from "@/components/TimeLine/TimeLineCard.vue";
 import { ref } from "vue";
 
 import type { Ref } from "vue";
 
-const etats: { name: string, value: StockState }[] = [{
-  'name': 'Tout',
-  value: StockState.ALL
-}, {
-  'name': 'Approvisionnement',
-  value: StockState.SUPPLY
-}, {
-  'name': 'Vente',
-  value: StockState.SALE
-}]
+const etats: { name: string; value: StockState }[] = [
+  {
+    name: "Tout",
+    value: StockState.ALL,
+  },
+  {
+    name: "Approvisionnement",
+    value: StockState.SUPPLY,
+  },
+  {
+    name: "Vente",
+    value: StockState.SALE,
+  },
+];
 
-const activeTab: Ref<StockState> = ref(StockState.ALL)
+const activeTab: Ref<StockState> = ref(StockState.ALL);
 </script>
 <style scoped></style>
