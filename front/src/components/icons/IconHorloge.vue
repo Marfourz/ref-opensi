61fa8b9cd9531fbe2a116c534e395fdf950ef93e
Arif Stephane <hounkpatinstepane@gmail.com>
2023-03-19 21:00:15 +0100

    fix(font):composant VHistorical for historic

---
+++ front/src/components/icons/IconHorloge.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10 2.50003C5.85937 2.50003 2.5 5.85941 2.5 10C2.5 14.1407 5.85937 17.5 10 17.5C14.1406 17.5 17.5 14.1407 17.5 10C17.5 5.85941 14.1406 2.50003 10 2.50003Z"
+      stroke="#6D7580"
+      stroke-miterlimit="10"
+    />
+    <path
+      d="M10 5.00003V10.625H13.75"
+      stroke="#6D7580"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
