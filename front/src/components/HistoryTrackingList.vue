ab946561d80021c44c367f507102b60ccafc4dac
Stephane Arif <hounkpatinstepane@gmail.com>
2023-03-21 20:47:49 +0000

    path orders history

--- front/src/components/HistoryTrackingList.vue
+++ front/src/components/HistoryTrackingList.vue
@@ -2,43 +2,64 @@
   <div class="py-8 px-6">
     <div class="flex justify-between items-center mb-8">
       <div class="text-2xl font-semibold">Historique</div>
-      <BaseIcon
-        name="close"
-        class="w-5 h-5 cursor-pointer"
-        @click="close"
-      ></BaseIcon>
+      <BaseIcon name="close" class="w-5 h-5 cursor-pointer"></BaseIcon>
     </div>
     <div class="flex mt-8">
       <div class="font-semibold text-base">Commande</div>
       <!-- <BaseTableStatut :title="order.status" class="ml-2"></BaseTableStatut>  -->
+    </div>
+    <div class="mt-4">
+      <history-tracking
+        type="created"
+        :date="order_created.date"
+        v-if="order_created"
+      ></history-tracking>
+    </div>
+    <div class="mt-4">
+      <history-tracking
+        type="validated"
+        :date="order_accepted.date"
+        v-if="order_accepted"
+      ></history-tracking>
+    </div>
+    <div class="mt-4">
+      <history-tracking
+        type="submitted"
+        :date="order_delivered.date"
+        v-if="order_delivered"
+      ></history-tracking>
     </div>
     <!-- <div class="mt-4"><history-tracking :type="validated" :date="order.updatedAt"></history-tracking></div>  -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, PropType, ref } from "vue";
 import HistoryTracking from "@/components/HistoryTracking.vue";
+import { IOrderHistory } from "@/types/interfaces";
+import { IOrder } from "@/types/interfaces";
+import { useOrdersStore } from "@/stores/orders";
 
-const showModal = ref(false);
- function close(){
-  showModal.value =true;
- };
 export default defineComponent({
   components: { HistoryTracking },
+
   props: {
-    // orders: {
-    //   type: String,
-    // },
+    order_created: Object as PropType<IOrderHistory>,
+    order_accepted: Object as PropType<IOrderHistory>,
+    order_delivered: Object as PropType<IOrderHistory>,
+    orderId: { type: String, required: true},
   },
 
   setup(props) {
-    onMounted(async () => {});
+    const orderStore = useOrdersStore();
+    const infoHistoryOrder = ref();
 
-    return {
-      showModal,
-      close,
-    };
+    onMounted(async () => {
+      const response = await orderStore.historyOrder(props.orderId);
+      infoHistoryOrder.value = response;
+    });
+
+    return {};
   },
 });
 </script>
